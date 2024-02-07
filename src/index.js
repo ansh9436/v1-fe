1bf293658f9db47e8fbe6ad2f6231a9a42cfcaa4
ansh9436 <ansh9436@gmail.com>
2024-02-07 16:44:42 +0900

    처음 업로드

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import {BrowserRouter} from "react-router-dom";
+import {Provider} from "react-redux";
+import store from "./redux/configStore";
+import {PersistGate} from "redux-persist/integration/react";
+import persistStore from "redux-persist/es/persistStore";
+
+const persistor = persistStore(store)
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <Provider store={store}>
+        <PersistGate persistor={persistor}>
+            <BrowserRouter>
+                <App/>
+            </BrowserRouter>
+        </PersistGate>
+    </Provider>
+);
