230592796e33fcd9c01f31b405e9c041838809b0
ansh9436 <ansh9436@gmail.com>
2024-02-27 17:02:31 +0900

    회원가입 중복검사 완료

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,10 +1,10 @@
-import React, { /*useState*/ } from "react";
+import React from "react";
 import axios from "axios";
 import Header from "../components/Common/Header";
 import RegisterInput from "../components/Register/RegisterInput";
 import StyledContainer from "../components/Style/StyledContainer";
 import StyledBox from "../components/Style/StyledBox";
-import {toast, ToastContainer} from "react-toastify";
+import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Formik } from "formik";
 import * as Yup from "yup";
@@ -14,52 +14,66 @@ import './Register.scss';
 
 const Register = () => {
     const navigate = useNavigate();
-    /*const [passEmail, setPassEmail] = useState(false);
-    const [passNick, setPassNick] = useState(false);
-
-    const checkEmail = (e) => {
-        if (passEmail) {
-            return false;
-        } else {
-            return checkUser('user_email', e.target.value);
-        }
-    }
-
-    const checkNick = (e) => {
-        if (passNick) {
-            return false;
-        } else {
-            return checkUser('user_nick', e.target.value);
-        }
-    }
-
-    const checkUser = (type, user_value) => {
-        axios.post(`/api/du_check/${type}`, { user_value: user_value })
-        .then((res) => {
-            console.log(res);
-            if(res.data.success) {
-                if(type === 'user_email') {
-                    setPassEmail(true);
-                } else if(type === 'user_nick') {
-                    setPassNick(true);
-                } else {
-
-                }
-            } else {
-
-            }
-        })
-        .catch((err) => {
-            toast.error(err + "😭", {
-                position: "top-center",
+    const checkUser = async (type, user_value) => {
+        let isSuccess = false;
+        await axios.post(`/api/du_check/${type}`, {user_value: user_value})
+            .then((res) => {
+                console.info(`${type} 중복검사 ajax 결과값`, res.data);
+                isSuccess = !!res.data.success;
+            })
+            .catch((err) => {
+                console.info(`${type} 중복검사 ajax 에러`, err);
+                isSuccess = false;
             });
-        });
-    }*/
+        return isSuccess;
+    }
 
+    const userEmailCheck = function (){
+        return this.test("userEmailCheck", async function (userValue) {
+            let result;
+            const {path, createError} = this;
+            const regEmailExp = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+            console.log('이메일 넘어온 값', userValue);
+            if (userValue !== undefined && regEmailExp.test(userValue)) {
+                return new Promise(async (resolve) => {
+                    const returnValue = await checkUser('user_email', userValue);
+                    if (returnValue) {
+                        result = true;
+                    } else {
+                        result = createError({path, message: "동일한 이메일 주소가 존재합니다."});
+                    }
+                    resolve(result);
+                });
+            } else {
+                return createError({path, message: "올바른 닉네임을 입력해 주세요!"});
+            }
+        });
+    };
+
+    function userNickCheck() {
+        return this.test("userNickCheck", async function (userValue) {
+            let result;
+            const {path, createError} = this;
+            const regNickExp = /^[가-힣a-zA-Z][^!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?\s]*$/;
+            console.log('닉네임 넘어온 값', userValue);
+            if (userValue !== undefined && regNickExp.test(userValue)) {
+                return new Promise(async (resolve) => {
+                    result = await checkUser('user_nick', userValue) ?
+                        true : createError({path, message: "동일한 닉네임이 존재합니다."});
+                    resolve(result);
+                });
+            } else {
+                return createError({path, message: "올바른 이메일 주소를 입력해 주세요!"});
+            }
+        });
+    }
+
+    Yup.addMethod(Yup.string, "userEmailCheck", userEmailCheck);
+    Yup.addMethod(Yup.string, "userNickCheck", userNickCheck);
     const validationSchema = Yup.object().shape({
         user_email: Yup.string()
-            .email("올바른 이메일 형식이 아닙니다!")
-            .required("이메일을 입력하세요!"),
+            .userEmailCheck()
+            .required("올바른 이메일 주소를 입력해 주세요!!"),
         user_passwd: Yup.string()
             .min(8, "비밀번호는 최소 8자리 이상입니다")
             .max(16, "비밀번호는 최대 16자리입니다!")
@@ -74,14 +88,11 @@ const Register = () => {
         user_nick: Yup.string()
             .min(2, "닉네임은 최소 2글자 이상입니다!")
             .max(10, "닉네임은 최대 10글자입니다!")
-            .matches(
-                /^[가-힣a-zA-Z][^!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?\s]*$/,
-                "닉네임에 특수문자가 포함되면 안되고 숫자로 시작하면 안됩니다!"
-            )
+            .userNickCheck()
             .required("닉네임을 입력하세요!"),
     });
     const submit = async (values) => {
-        const { user_email, user_passwd, user_nick } = values;
+        const {user_email, user_passwd, user_nick} = values;
         try {
             await axios.post("/api/join", {
                 user_email: user_email,
@@ -89,12 +100,12 @@ const Register = () => {
                 user_nick: user_nick,
             }).then(res => {
                 console.log('res', res);
-                if(res.data.success) {
+                if (res.data.success) {
                     toast.success(<h3>회원가입이 완료되었습니다.<br/>로그인 하세요😎</h3>, {
                         position: "top-center",
                         autoClose: 2000
                     });
-                    setTimeout(()=> {
+                    setTimeout(() => {
                         navigate("/login");
                     }, 2000);
                 } else {
@@ -103,7 +114,7 @@ const Register = () => {
                     });
                 }
             });
-        } catch(e) {
+        } catch (e) {
             toast.error(e.response.data.message + "😭", {
                 position: "top-center",
             });
@@ -125,7 +136,7 @@ const Register = () => {
                 <StyledContainer>
                     <ToastContainer/>
                     <div>
-                        <Header link={"./"} title="회원가입" isBackButton={true} />
+                        <Header link={"./"} title="회원가입" isBackButton={true}/>
                         <StyledBox padding="18px 16px" lineHeight="20px">
                             <form onSubmit={handleSubmit}>
                                 <RegisterInput
@@ -166,7 +177,7 @@ const Register = () => {
                                 </div>
                                 <RegisterInput
                                     labelName="닉네임"
-                                    name="userNickname"
+                                    name="user_nick"
                                     type="text"
                                     placeholder="닉네임"
                                     onChange={handleChange}
